8ca8596862a05af31cb453875e349230401b6f82
claire mitchell <clairelouisemitchell1@gmail.com>
2017-07-31 12:13:47 +0100

    first commit

---
+++ hello/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
